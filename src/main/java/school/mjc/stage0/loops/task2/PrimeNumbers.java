7ac51b61c68d9849600a4f4c07c40fede582a0d3
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-24 22:00:39 +0500

    check mistakes

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
         int counter=0;
-        while (counter<printToInclusive){
+        while (counter<=printToInclusive){
             System.out.println(counter);
             counter++;
         }
